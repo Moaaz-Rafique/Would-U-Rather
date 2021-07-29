b5244f0a682174df2cddfdd6d29ea55a52a82bd4
Moaaz-Rafique <moaazrafiquetk@gmail.com>
2021-07-29 17:01:10 +0500

    add hover for number of votes on option after votes

--- src/components/PollCard.js
+++ src/components/PollCard.js
@@ -1,36 +1,34 @@
 import React, { useState, useEffect } from "react";
 import Paper from "@material-ui/core/Paper";
-import { Button, Typography,Collapse } from "@material-ui/core";
+import { Button, Typography, Collapse } from "@material-ui/core";
 
-
-
-function PollCard({ poll, submitVote, index, deletePoll,currentUser,users }) {
+function PollCard({ poll, submitVote, index, deletePoll, currentUser, users }) {
   const { statement, options, votes, creator } = poll;
   const [selectedOption, setSelectedOption] = useState(0);
-  // const [canVote,setCanVote]=useState(true)
+
+  const [hoveredOption, setHoveredOption] = useState(-1);
   const onChangeValue = (e) => {
     setSelectedOption(e.target.value);
   };
-  
+
   let noOfVotes = votes.length;
-  const optionVotes=[]
-  let voted=false
+  const optionVotes = [];
+  let voted = false;
   let myVote;
-  for(let i in options){
-    let temp=0;
-    for(let j of votes){      
-      if(i==j.option){
-        temp++
+  for (let i in options) {
+    let temp = 0;
+    for (let j of votes) {
+      if (i == j.option) {
+        temp++;
       }
-      if(currentUser===j.user && !voted){
-        voted=true
-        myVote=options[j.option]
+      if (currentUser === j.user && !voted) {
+        voted = true;
+        myVote = options[j.option];
       }
     }
     optionVotes.push(temp);
   }
   return (
-    
     <Paper
       elevation={1}
       style={{ width: "60%", margin: "20px", padding: "20px" }}
@@ -54,41 +52,54 @@ function PollCard({ poll, submitVote, index, deletePoll,currentUser,users }) {
               onChangeValue(e);
             }}
           >
-            <label>
+            <label
+              onMouseEnter={() => {
+                setHoveredOption(index);
+              }}
+              onMouseLeave={() => {
+                setHoveredOption(-1);
+              }}
+            >
               {" "}
-              {(submitVote && !voted) ? (
+              {submitVote && !voted ? (
                 <input type="radio" name={statement} value={index} />
               ) : (
                 ""
               )}
-              <span style={{color: (voted && myVote==opt) ? '#400CCC' : 'black'}} >
+              <span
+                style={{ color: voted && myVote == opt ? "#400CCC" : "black" }}
+              >
                 {opt.name}
-                <sub style={{fontSize: '8px'}}> {optionVotes[index] ? optionVotes[index]:''} </sub>
+                <sub style={{ fontSize: "11px" }}>
+                  {" "}
+                  {hoveredOption == index && voted
+                    ? optionVotes[index]
+                    : ""}{" "}
+                </sub>
               </span>
             </label>
           </div>
         );
       })}
-      {
-        deletePoll ? (
-          <Button
-            variant="outlined"
-            style={{
-              marginTop: "20px",
-              padding: "10px",
-            }}
-            onClick={(index) => {
-              deletePoll(index, selectedOption);
-            }}
-          >
-            Delete Poll
-          </Button>
-        ): ''
-      } 
+      {deletePoll ? (
+        <Button
+          variant="outlined"
+          style={{
+            marginTop: "20px",
+            padding: "10px",
+          }}
+          onClick={(index) => {
+            deletePoll(index, selectedOption);
+          }}
+        >
+          Delete Poll
+        </Button>
+      ) : (
+        ""
+      )}
 
-
-    <Collapse in={submitVote && !voted}>
-      <Button
+      <Collapse in={submitVote && !voted}>
+        <Button
           variant="outlined"
           style={{
             marginTop: "20px",
@@ -100,9 +111,11 @@ function PollCard({ poll, submitVote, index, deletePoll,currentUser,users }) {
         >
           Submit Vote
         </Button>
-    </Collapse>
+      </Collapse>
       <div>
-        <Typography variant='caption' color='secondary'>Poll by: {users[creator].name}</Typography>
+        <Typography variant="caption" color="secondary">
+          Poll by: {users[creator].name}
+        </Typography>
       </div>
     </Paper>
   );
